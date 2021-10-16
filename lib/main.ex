a208bf4eef625e8179ff77252e2198eb91623897
ezmicken <smoggach@gmail.com>
2021-10-16 15:42:57 -0400

    add the otp concurrency queue example from elixirschool

--- lib/main.ex
+++ lib/main.ex
@@ -1,6 +1,5 @@
 defmodule Smoggach.RUN do
   def main(args \\ []) do
-    Smoggach.annagrams?(Enum.at(args, 0), Enum.at(args, 1))
-    |> IO.puts()
+    Smoggach.annagrams?(Enum.at(args, 0), Enum.at(args, 1)) |> IO.puts()
   end
 end
